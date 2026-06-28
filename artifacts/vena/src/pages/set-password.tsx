12fa981b73c1a39234bc0382b1485da64147bf39
catherenelarissa-code <catherenelarissa@gmail.com>
2026-06-28 02:30:16 -0300

    Update set-password.tsx

--- artifacts/vena/src/pages/set-password.tsx
+++ artifacts/vena/src/pages/set-password.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+imprt { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { useLocation, Link, useSearch } from "wouter";
